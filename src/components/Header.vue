19ab7c7004a3bf275ff1751da1126a10f177d33a
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-08-03 19:03:27 -0400

    Iniciando projeto

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar app color="blue" hide-on-scroll>
+    <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">Rastreio Correios</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon color="white">mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon color="white">mdi-chevron-down</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
